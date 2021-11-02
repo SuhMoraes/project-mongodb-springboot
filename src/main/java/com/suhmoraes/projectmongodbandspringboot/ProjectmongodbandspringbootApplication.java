c491a64476ca746b19af3bfb0ced778d94614d60
SuhMoraes <suh.alvesmoraes@gmail.com>
2021-11-02 12:46:27 -0300

    Project created, port change, insert .idea in gitignore and yaml

---
+++ src/main/java/com/suhmoraes/projectmongodbandspringboot/ProjectmongodbandspringbootApplication.java
@@ -0,0 +1,13 @@
+package com.suhmoraes.projectmongodbandspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectmongodbandspringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectmongodbandspringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/suhmoraes/projectmongodbandspringboot/ProjectmongodbandspringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suhmoraes.projectmongodbandspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectmongodbandspringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
